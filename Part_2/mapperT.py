9bff2d4ed45cb3bc8112c8e2fcf2955a29dcef2a
zhiwei <zhli3769@student.uu.se>
2018-04-23 20:57:16 +0000

    It works for some reason. :)

--- Part_2/mapperT.py
+++ Part_2/mapperT.py
@@ -6,10 +6,10 @@ import json
 
 # input comes from STDIN (standard input) load into json
 for line in sys.stdin:
-   # jsonStr = line.decode("utf-8")
 	try:
 		data = json.loads(line)
 		text = json.dumps(data['text'])
 		print (text)
+		print ("\n")
 	except:
 		continue
