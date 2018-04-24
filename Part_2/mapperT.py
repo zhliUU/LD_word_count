cd9208aa2a2c8c6bf1a1db1786334a53c96d09a3
zhiwei <zhli3769@student.uu.se>
2018-04-24 18:44:25 +0000

    mapper for tweets

--- Part_2/mapperT.py
+++ Part_2/mapperT.py
@@ -1,15 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """mapperT.py"""
 
 import sys
 import json
-
+pronouns = ['han','hon','den','det','denna','denne','hen']
 # input comes from STDIN (standard input) load into json
 for line in sys.stdin:
 	try:
 		data = json.loads(line)
 		text = json.dumps(data['text'])
-		print (text)
-		print ("\n")
+		#print (text)
+		text = text.strip().lower()
+		words = text.split()
+		for word in words:
+			if (word in pronouns):
+				print ('%s\t %s' % (word, 1))
+		#print('\n')
 	except:
 		continue
